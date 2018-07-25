20eaa39960164bc46e48c088b0bf8979a0257a02
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-25 20:55:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  return drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+  return drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  return drivers.shift(name)
+}
+
+function appendDriver(name) {
+  return [...drivers, name]
+}
